e515cb806b45ec967599ea5d1d81ba767c939c20
Vivek <vivek.mitra@outlook.com>
2023-07-08 11:06:34 +1000

    js file added and initial comments added

---
+++ odin-script.js
@@ -0,0 +1,5 @@
+//This script serves to manipulate the DOM of the odin form. The code is aiming
+//to show the default screen on start up which just has the #about with YES or NO
+//If the user clicks on no, they get the #good-luck
+//if user clicks on yes, they get the form
+//If user clicks submit in the #odin-form, they will get a prompt
