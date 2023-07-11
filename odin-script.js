8bcd1ff83ff9354320978d78d718f5d4f0f1c4f7
Vivek <vivek.mitra@outlook.com>
2023-07-11 19:24:12 +1000

    basic JS added to check how a blank slate looks like

--- odin-script.js
+++ odin-script.js
@@ -3,3 +3,22 @@
 //If the user clicks on no, they get the #good-luck
 //if user clicks on yes, they get the form
 //If user clicks submit in the #odin-form, they will get a prompt
+
+
+//initially taking all the major structural components into our js constants. This will help us attach/modify/resize w/ CSS
+const logo = document.querySelector('#logo');
+const about = document.querySelector('#about-container');
+const goodLuck = document.querySelector('#good-luck');
+const formContainer = document.querySelector('#form-container');
+
+
+function defaultView () {                        //giving a blank slate to work with - so we can manipulate
+    let defaultBody = document.body;
+    defaultBody.innerHTML = '';
+}
+
+function logoEntry() {
+    
+}
+
+
